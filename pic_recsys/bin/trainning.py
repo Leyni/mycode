91110bbef32fd0530edbde919369200f7d3a3f35
kongfanyang <kongfanyang@ke.com>
2018-07-29 11:47:58 +0800

    update tranning

--- pic_recsys/bin/trainning.py
+++ pic_recsys/bin/trainning.py
@@ -2,14 +2,14 @@ import sys
 import logging
 import json
 from gensim.models import Word2Vec
-import xgboost
+import xgboost as xgb
 
 err = None
 
 debug_value = {}
 
 # config
-work_path = '/Users/kongfanyang/Develop/mycode/pic_recsys'
+work_path = '/home/kongfanyang/git/mycode/pic_recsys'
 log_path = work_path + '/log'
 etc_path = work_path + '/etc'
 # config end
@@ -40,16 +40,18 @@ class KW(object):
         print err_cnt, err_pos_cnt
 
 class Data(object):
+    def __init__(self, part):
+        self.part = part
     def __iter__(self):
         err_cnt = 0
         err_pos_cnt = 0
         cnt = 0
-        f = open(etc_path + '/query_result.tsv', 'r')
+        f = open(etc_path + '/query_result.txt' + self.part, 'r')
         l = f.readline()
         l = f.readline()
         while l:
             cnt += 1
-            if cnt % 1000 == 0 : print cnt
+            #if cnt % 1000 == 0 : print cnt
             cols = l.strip().split('\t')
             label = cols[0]
             try:
@@ -57,7 +59,7 @@ class Data(object):
                 yield label, kws
             except Exception, e:
                 err_cnt += 1
-                if cols[0] == 1: err_pos_cnt += 1
+                if label == 1: err_pos_cnt += 1
             l = f.readline()
         print err_cnt, err_pos_cnt
 
@@ -66,9 +68,9 @@ def tranning_word2vec_model():
     model = Word2Vec(kws, size=1000, window = 10)
     model.save(etc_path + "/word_vec.model")
 
-def item2vec():
-    kws_vec = Data()
-    out_file = open(etc_path + '/data_vec.txt', 'w')
+def item2vec(part):
+    kws_vec = Data(part)
+    out_file = open(etc_path + '/data_vec.txt' + part, 'w')
     model = Word2Vec.load(etc_path + "/word_vec.model")
     for label, kws in kws_vec:
         item_vec = None
@@ -83,14 +85,14 @@ def item2vec():
                     item_vec += model[kw]
                 except:
                     pass
-        out_file.write('%s' % label)
-        for i in range(len(item_vec)):
-            out_file.write(' %d:%s' % (i, item_vec[i]))
-        out_file.write('\n')
+        if item_vec is not None:
+            out_file.write('%s' % label)
+            for i in range(len(item_vec)):
+                out_file.write(' %d:%s' % (i, item_vec[i]))
+            out_file.write('\n')
 
 def train_classifier():
-    dtrain = xgb.DMatrix('demo/data/agaricus.txt.train')
-    dtest = xgb.DMatrix('demo/data/agaricus.txt.test')
+    dtrain = xgb.DMatrix(etc_path + '/data_vec.txt')
     # specify parameters via map
     param = {'max_depth':2, 'eta':1, 'silent':0, 'objective':'binary:logistic' }
     num_round = 2
@@ -99,7 +101,10 @@ def train_classifier():
     bst.save_model(etc_path + '/xgbt.model')
 
 #tranning_word2vec_model()
-item2vec()
+
+#part = sys.argv[1]
+#item2vec(part)
+train_classifier()
 
 # main
 
