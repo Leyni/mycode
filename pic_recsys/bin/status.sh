447b9a46775b1bd1c3c0504295920ed1c4f3cdaa
leyni <leyni@126.com>
2019-02-16 13:38:33 +0800

    move code

--- pic_recsys/bin/status.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mysql -uleyni -p -e "select rec_status,count(*) as cnt from pic_rec.sample_e621_set group by rec_status;;select label_status, count(1) as cnt from pic_rec.sample_e621_set group by label_status;"
